888034ca06f3f875f9c0abc6c1081d174f50f416
Kristofer <kristofer15@ru.is>
2018-09-06 17:18:17 +0000

    Cleaned up main. Added dynamic flag settings

--- newClient.cpp
+++ newClient.cpp
@@ -10,7 +10,9 @@
 #include<netinet/tcp.h>   //Provides declarations for tcp header
 #include<netinet/ip.h>    //Provides declarations for ip header
 #include<unistd.h>
+#include<time.h>
 
+// From: https://www.binarytides.com/tcp-syn-portscan-in-c-with-linux-sockets/
 struct pseudo_header    //needed for checksum calculation
 {
     unsigned int source_address;
@@ -22,6 +24,7 @@ struct pseudo_header    //needed for checksum calculation
     struct tcphdr tcp;
 };
 
+// From: https://www.binarytides.com/tcp-syn-portscan-in-c-with-linux-sockets/
 unsigned short csum(unsigned short *ptr,int nbytes)
 {
     register long sum;
@@ -46,6 +49,7 @@ unsigned short csum(unsigned short *ptr,int nbytes)
     return(answer);
 }
 
+// Modified from: https://www.binarytides.com/tcp-syn-portscan-in-c-with-linux-sockets/
 uint32_t get_local_address()
 {
 
@@ -69,44 +73,87 @@ uint32_t get_local_address()
     struct sockaddr_in name;
     socklen_t namelen = sizeof(name);
     err = getsockname(sock, (struct sockaddr*) &name, &namelen);
- 
-    // const char *p = inet_ntop(AF_INET, &name.sin_addr, buffer, 100);
-
     close(sock);
+
     return name.sin_addr.s_addr;
 }
 
-int main(int argc, char* argv[]) {
+struct iphdr* get_ip_header(char *datagram, uint32_t source_address, uint32_t destination_address) {
+    struct iphdr *iph = (struct iphdr *) datagram;
 
+    //Fill in the IP Header
+    iph->ihl = 5;
+    iph->version = 4;
+    iph->tos = 0;
+    iph->tot_len = sizeof (struct ip) + sizeof (struct tcphdr);
+    iph->id = htons (3525); //Id of this packet
+    iph->frag_off = htons(16384);
+    iph->ttl = 20;
+    iph->protocol = IPPROTO_TCP;
+    iph->check = 0;      //Set to 0 before calculating checksum
+    iph->saddr = source_address; // source_in.sin_addr.s_addr;
+    iph->daddr = destination_address;
+     
+    iph->check = csum ((unsigned short *) datagram, iph->tot_len >> 1);
+
+    return iph;
+}
+
+// flag logic from:
+// https://github.com/chinmay29/PortScanner/blob/master/PortScanner.cpp
+struct tcphdr* get_tcp_header(char *datagram, uint32_t destination_port, uint8_t flags) {
+    struct tcphdr *tcph = (struct tcphdr *) (datagram + sizeof (struct iphdr));
+
+    //TCP Header
+    srand(time(NULL));  // Seed random function
+    tcph->source = htons((rand() % 2000) + 2000); // Get a random port in range 1999 - 3999
+
+    tcph->dest = destination_port;
+    tcph->seq = htonl(3434);
+    tcph->ack_seq = 0;
+    tcph->doff = sizeof(struct tcphdr) / 4;      //Size of tcp header
+    tcph->fin = TH_FIN & flags ? 1 : 0;
+    tcph->syn = TH_SYN & flags ? 1 : 0;
+    tcph->rst = TH_RST & flags ? 1 : 0;
+    tcph->psh = TH_PUSH & flags ? 1 : 0; // Why are you like this!?
+    tcph->ack = TH_ACK & flags ? 1 : 0;
+    tcph->urg = TH_URG & flags ? 1 : 0;
+    tcph->window = htons ( 14600 );  // maximum allowed window size
+    tcph->check = 0;
+    tcph->urg_ptr = 0;
+
+    return tcph;
+}
+
+void disable_os_header(int s) {
+    int one = 1;
+    const int *val = &one;
+     
+    if (setsockopt (s, IPPROTO_IP, IP_HDRINCL, val, sizeof (one)) < 0)
+    {
+        printf ("Error setting IP_HDRINCL. Error number : %d . Error message : %s \n" , errno , strerror(errno));
+        exit(0);
+    }
+}
+
+int main(int argc, char* argv[]) {
     
     if (argc < 3) {
        fprintf(stderr,"usage %s hostname port\n", argv[0]);
        exit(0);
     }
 
-	int s = socket(AF_INET, SOCK_RAW, IPPROTO_TCP);
-    int d = socket(AF_INET, SOCK_RAW, IPPROTO_TCP);
-
-    //Datagram to represent the packet
-    char datagram[4096];    
-     
-    //IP header
-    struct iphdr *iph = (struct iphdr *) datagram;
-     
-    //TCP header
-    struct tcphdr *tcph = (struct tcphdr *) (datagram + sizeof (struct iphdr));
-
     struct sockaddr_in source_in, destination_in;
     struct pseudo_header psh;
     struct hostent *source;
     struct hostent *destination;
 
-    // Get source address
-    uint32_t source_address = get_local_address();
+	int s = socket(AF_INET, SOCK_RAW, IPPROTO_TCP);
+    int d = socket(AF_INET, SOCK_RAW, IPPROTO_TCP);
 
-    // https://stackoverflow.com/questions/7059299/how-to-properly-convert-an-unsigned-char-array-into-an-uint32-t
-    // uint32_t source_address = 0;
-    // memcpy(&source_address, source_buffer, 4);
+    //Datagram to represent the packet
+    char datagram[4096];
+    memset (&datagram, 0, 4096); /* zero out the buffer */
 
     // Get destination hostent
     destination = gethostbyname(argv[1]);
@@ -114,6 +161,9 @@ int main(int argc, char* argv[]) {
         (char *)&destination_in.sin_addr.s_addr,
         destination->h_length);
 
+    // Get source address
+    uint32_t source_address = get_local_address();
+    uint32_t destination_address = destination_in.sin_addr.s_addr;
 
     if (destination == NULL) {
         fprintf(stderr,"ERROR, no such host\n");
@@ -126,57 +176,14 @@ int main(int argc, char* argv[]) {
     // source_in.sin_port = htons(3123);
     destination_in.sin_port = htons(atoi(argv[2]));
 
-    // printf("Source address: %d\n", source->h_addr);
-    printf("Destination address: %d\n", destination->h_addr);
+    //IP header
+    struct iphdr *iph = get_ip_header(datagram, source_address, destination_address);
 
-    // printf("Local source IP is %s \n" , source_in.sin_port);
+    //TCP header
+    struct tcphdr *tcph = get_tcp_header(datagram, destination_in.sin_port, TH_SYN);
 
-    memset (&datagram, 0, 4096); /* zero out the buffer */
-
-    //Fill in the IP Header
-    iph->ihl = 5; // TRY CHANGING TO 4
-    iph->version = 4;
-    iph->tos = 0;
-    iph->tot_len = sizeof (struct ip) + sizeof (struct tcphdr);
-    iph->id = htons (3525); //Id of this packet
-    iph->frag_off = htons(16384);
-    iph->ttl = 20;
-    iph->protocol = IPPROTO_TCP;
-    iph->check = 0;      //Set to 0 before calculating checksum
-    iph->saddr = source_address; // source_in.sin_addr.s_addr;
-    iph->daddr = destination_in.sin_addr.s_addr;
-     
-    iph->check = csum ((unsigned short *) datagram, iph->tot_len >> 1);
-     
-    //TCP Header
-    tcph->source = htons(3123);
-    tcph->dest = destination_in.sin_port;
-    tcph->seq = htonl(3434);
-    tcph->ack_seq = 0;
-    tcph->doff = sizeof(struct tcphdr) / 4;      //Size of tcp header
-    tcph->fin=0;
-    tcph->syn=1;
-    tcph->rst=0;
-    tcph->psh=0;
-    tcph->ack=0;
-    tcph->urg=0;
-    tcph->window = htons ( 14600 );  // maximum allowed window size
-    // tcph->check = 0; //if you set a checksum to zero, your kernel's IP stack should fill in the correct checksum during transmission
-    tcph->urg_ptr = 0;
-
-    int one = 1;
-    const int *val = &one;
-     
-    if (setsockopt (s, IPPROTO_IP, IP_HDRINCL, val, sizeof (one)) < 0)
-    {
-        printf ("Error setting IP_HDRINCL. Error number : %d . Error message : %s \n" , errno , strerror(errno));
-        exit(0);
-    }
-
-    // tcph->dest = htons ( port );
-
-    psh.source_address = iph->saddr; //source_in.sin_addr.s_addr;
-    psh.dest_address = destination_in.sin_addr.s_addr;
+    psh.source_address = source_address;
+    psh.dest_address = destination_address;
     psh.placeholder = 0;
     psh.protocol = IPPROTO_TCP;
     psh.tcp_length = htons( sizeof(struct tcphdr) );
@@ -184,7 +191,8 @@ int main(int argc, char* argv[]) {
     memcpy(&psh.tcp , tcph , sizeof (struct tcphdr));
 
     tcph->check = csum( (unsigned short*) &psh , sizeof (struct pseudo_header));
-        
+
+    disable_os_header(s);
     //Send the packet
     printf("Sending the thing\n");
     if ( sendto (s, datagram, sizeof(struct iphdr) + sizeof(struct tcphdr) , 0 , (struct sockaddr *) &destination_in, sizeof (destination_in)) < 0)
