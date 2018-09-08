89797c9e77cf2b8ba786e72c3a70c62335ebf1d8
Kristofer <kristofer15@ru.is>
2018-09-08 19:29:11 +0000

    Structure changes

--- client.cpp
+++ client.cpp
@@ -1,21 +1,26 @@
-#include<stdio.h> //printf
-#include<string.h> //memset
-#include<stdlib.h> //for exit(0);
-#include<sys/socket.h>
-#include<errno.h> //For errno - the error number
-#include<netdb.h> //hostend
-#include<arpa/inet.h>
-#include<netinet/tcp.h>   //Provides declarations for tcp header
-#include<netinet/ip.h>    //Provides declarations for ip header
-#include<unistd.h>
-#include<time.h>
+#include <stdio.h> //printf
+#include <string.h> //memset
+#include <stdlib.h> //for exit(0);
+#include <sys/socket.h>
+#include <errno.h> //For errno - the error number
+#include <netdb.h> //hostend
+#include <arpa/inet.h>
+#include <netinet/tcp.h>   //Provides declarations for tcp header
+#include <netinet/ip.h>    //Provides declarations for ip header
+#include <unistd.h>
+#include <time.h>
 
-#include<thread>
+#include <thread>
+#include <mutex>
 #include <vector>
+#include <list>
 #include <algorithm>    // std::random_shuffle
 #include <iostream>
 
 #include "file_reader.h"
+
+// TODO: CHANGE ME PROBABLY
+std::mutex PORT_MUTEX;
 
 // From: https://www.binarytides.com/tcp-syn-portscan-in-c-with-linux-sockets/
 struct pseudo_header    //needed for checksum calculation
@@ -52,6 +57,25 @@ unsigned short csum(unsigned short *ptr,int nbytes)
     answer=(short)~sum;
      
     return(answer);
+}
+
+// TODO: templates
+void exclusive_push(std::list<int> &l, int value) {
+    std::lock_guard<std::mutex> guard(PORT_MUTEX);
+    l.push_back(value);
+}
+
+void exclusive_remove(std::list<int> &l, int value) {
+    std::lock_guard<std::mutex> guard(PORT_MUTEX);
+    l.remove(value);
+}
+
+void exclusive_print(std::list<int> l) {
+    std::list<int>::iterator it;
+    
+    for(it = l.begin(); it != l.end(); ++it) {
+        std::cout << *it << std::endl;
+    }
 }
 
 // Modified from: https://www.binarytides.com/tcp-syn-portscan-in-c-with-linux-sockets/
@@ -140,7 +164,7 @@ void disable_os_header(int s) {
     }
 }
 
-void analyze_response(char *datagram, uint32_t server_address, uint8_t desired_flags) {
+void analyze_response(char *datagram, uint32_t server_address, std::list<int> &hit_ports, uint8_t desired_flags) {
     struct iphdr *iph = (struct iphdr *) datagram;
     struct tcphdr *tcph = (struct tcphdr *) (datagram + sizeof(iphdr));
 
@@ -153,21 +177,25 @@ void analyze_response(char *datagram, uint32_t server_address, uint8_t desired_f
     received_flags |= tcph->fin ? TH_FIN : 0;
     received_flags |= tcph->rst ? TH_RST : 0;
 
-    if(ntohs(tcph->source) == 631) {
-        std::cout << "Port 631 RST: " << std::endl;
-        std::cout << (received_flags & TH_RST) << std::endl;
-    }
+    if(server_address == received_address) {
 
-    // received_flags are desired_flags and the address is correct
-    if((received_flags == desired_flags) && (server_address == received_address)) {
-        printf("%d is open\n", ntohs(tcph->source));
-    }
-    else if(server_address == received_address) {
-        // printf("%d is closed\n", ntohs(tcph->dest));
+        if(ntohs(tcph->source == 631))  {
+            std::cout << "Received 631" << std::endl;
+        }
+
+        exclusive_remove(hit_ports, ntohs(tcph->source));
+
+        // received_flags are desired_flags and the address is correct
+        if((received_flags == desired_flags) && (server_address == received_address)) {
+            printf("%d is open\n", ntohs(tcph->source));
+        }
+        else if(server_address == received_address) {
+            // printf("%d is closed\n", ntohs(tcph->dest));
+        }
     }
 }
 
-void sniff(uint32_t server_address, uint8_t desired_flags) {
+void sniff(uint32_t server_address, std::list<int> &hit_ports, uint8_t desired_flags) {
     int response_size;
 
     char datagram[4096];
@@ -191,44 +219,23 @@ void sniff(uint32_t server_address, uint8_t desired_flags) {
             response_size = recvfrom(s, datagram, sizeof(datagram), 0, NULL, NULL);
 
             if(response_size > 0) {
-                analyze_response(datagram, server_address, desired_flags);
+                analyze_response(datagram, server_address, hit_ports, desired_flags);
             }
         }
         else {
+            std::cout << "No response received from:" << std::endl;
+            exclusive_print(hit_ports);
             return;
         }
     }
 }
 
-void create_flags(int option, uint8_t &out, uint8_t &in) {
-
-    
-    switch(option) {
-        case 's':   // SYN scan
-            out = TH_SYN;
-            in = TH_SYN | TH_ACK;
-            return;
-        case 'n':   // NULL scan
-            out = 0;
-            in = 0;
-            return;
-        case 'x':   // XMAS scan
-            out = 0b11111111;
-            in = 0;
-            return;
-        case 'f':
-            out = TH_FIN;
-            in = 0;
-            return;
-        default:
-            error("Scan option not recognized");
-    }
-}
-
-void scan_host(const char *host_name, std::vector<int> &ports, int option) {
+void hit_tcp(const char *host_name, std::vector<int> &ports, uint8_t out_flags, uint8_t in_flags) {
     struct sockaddr_in source_in, destination_in;
     struct hostent *destination;
     struct pseudo_header psh;
+    std::mutex hit_port_mutex;
+    std::list<int> hit_ports;
 
 	int s = socket(AF_INET, SOCK_RAW, IPPROTO_TCP);
 
@@ -245,9 +252,6 @@ void scan_host(const char *host_name, std::vector<int> &ports, int option) {
     // Get source address
     uint32_t source_address = get_local_address();
     uint32_t destination_address = destination_in.sin_addr.s_addr;
-    
-    uint8_t out_flags, in_flags;
-    create_flags(option, out_flags, in_flags);
 
     if (destination == NULL) {
         fprintf(stderr,"ERROR, no such host\n");
@@ -264,12 +268,12 @@ void scan_host(const char *host_name, std::vector<int> &ports, int option) {
 
     disable_os_header(s);
 
-    std::thread sniffer_thread(sniff, destination_address, in_flags);
+    std::thread sniffer_thread(sniff, destination_address, std::ref(hit_ports), in_flags);
 
     srand(time(NULL));  // Seed random function
 
-    for(const int destination_port : ports) {        
-        tcph->source = htons((rand() % 2000) + 2000); // Get a random port in range 1999 - 3999
+    for(int destination_port : ports) {        
+        tcph->source = htons((rand() % 2000) + 2000); // Get a random port in range 2000 - 3999
         tcph->dest = htons(destination_port);
         tcph->check = 0;
             
@@ -277,21 +281,51 @@ void scan_host(const char *host_name, std::vector<int> &ports, int option) {
         psh.dest_address = destination_address;
         psh.placeholder = 0;
         psh.protocol = IPPROTO_TCP;
-        psh.tcp_length = htons( sizeof(struct tcphdr) );
+        psh.tcp_length = htons( sizeof(struct tcphdr));
             
-        memcpy(&psh.tcp , tcph , sizeof (struct tcphdr));
+        memcpy(&psh.tcp, tcph, sizeof(struct tcphdr));
 
         tcph->check = csum( (unsigned short*) &psh , sizeof (struct pseudo_header));
 
 
         if ( sendto (s, datagram, sizeof(struct iphdr) + sizeof(struct tcphdr) , 0 , (struct sockaddr *) &destination_in, sizeof (destination_in)) < 0)
         {
-            printf ("Error sending syn packet. Error number : %d . Error message : %s \n" , errno , strerror(errno));
+            printf ("Error sending packet. Error number: %d . Error message: %s \n" , errno , strerror(errno));
             exit(0);
         }
+
+        if(destination_port == 631) {
+            std::cout << "Added 631" << std::endl;
+        }
+
+        exclusive_push(hit_ports, destination_port);
     }
 
     sniffer_thread.join();
+}
+
+void scan_host(int option, const char *host_name, std::vector<int> &ports) {
+
+    switch(option) {
+        case 's':   // SYN scan
+            hit_tcp(host_name, ports, TH_SYN, TH_SYN|TH_ACK);
+            return;
+        case 'n':   // NULL scan
+            hit_tcp(host_name, ports, 0, 0);
+            // out = 0;
+            // in = 0;
+            return;
+        case 'x':   // XMAS scan
+            // out = 0b11111111;
+            // in = 0;
+            return;
+        case 'f':   // FIN scan
+            // out = TH_FIN;
+            // in = 0;
+            return;
+        default:
+            error("Scan option not recognized");
+    }
 }
 
 int main(int argc, char* argv[]) {
@@ -328,8 +362,8 @@ int main(int argc, char* argv[]) {
     std::random_shuffle(hosts.begin(), hosts.end());
 
     // TODO: Multithread me
-    for(const auto host : hosts) {
-        scan_host(host.c_str(), ports, option);
+    for(const std::string host : hosts) {
+        scan_host(option, host.c_str(), ports);
     }
 
     return 0;
